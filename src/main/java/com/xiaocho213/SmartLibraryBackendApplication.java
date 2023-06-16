01bd27b9c73b077bb2fc8f4cce777ad8b3f5f497
Zexu3366 <Zexu3366.163.com>
2023-06-16 17:14:47 +0800

    初始化项目

---
+++ src/main/java/com/xiaocho213/SmartLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiaocho213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLibraryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaocho213/SmartLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaocho213;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
