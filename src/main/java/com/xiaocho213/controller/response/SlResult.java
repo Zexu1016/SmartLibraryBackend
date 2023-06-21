ce80220c8b62239f83e48a53f0a2838b534a8735
Zexu3366 <Zexu3366.163.com>
2023-06-21 14:18:59 +0800

    完成回复的通信协议

--- src/main/java/com/xiaocho213/controller/response/SlResult.java
+++ src/main/java/com/xiaocho213/controller/response/SlResult.java
@@ -1,4 +1,26 @@
 package com.xiaocho213.controller.response;
 
-public class SlResult {
+
+
+
+public record SlResult <T> (T data, Integer statusCode, String message){
+    public static <T> SlResult<T> success(T data, String message) {
+        return new SlResult<>(data, 200, message);
+    }
+
+    public static <T> SlResult<T> success(T data) {
+        return success(data, null);
+    }
+
+    public static <T> SlResult<T> success() {
+        return success(null);
+    }
+
+    public static <T> SlResult<T> fail(T data, Integer statusCode, String message) {
+        return new SlResult<>(data, statusCode, message);
+    }
+
+    public static <T> SlResult<T> fail(Integer statusCode, String message) {
+        return fail(null, statusCode, message);
+    }
 }
